a2dd607f8b65d3a4ac138e7de647e5c3d4bd782c
pingproject27 <125154974+pingproject27@users.noreply.github.com>
2024-03-21 01:09:00 -0600

    Create LC_1757.sql

---
+++ LC_1757.sql
@@ -0,0 +1,7 @@
+-- 1757. Recyclable and Low Fat Products
+-- Write a solution to find the ids of products that are both low fat and recyclable.
+
+SELECT product_id
+FROM Products
+WHERE low_fats = 'Y'
+AND recyclable = 'Y'
