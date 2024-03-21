2a5299ac74d5855550aaeb849a523f2636c31484
pingproject27 <125154974+pingproject27@users.noreply.github.com>
2024-03-21 01:09:48 -0600

    Update SS_AverageSalaries.sql

--- SS_AverageSalaries.sql
+++ SS_AverageSalaries.sql
@@ -1,5 +1,6 @@
 -- Compare each employee's salary with the average salary of the corresponding department.
 -- Output the department, first name, and salary of employees along with the average salary of that department.
+-- Difficulty: Easy
 
 select department, first_name, salary, avg(salary) over(partition by department) as avg
 from employee
