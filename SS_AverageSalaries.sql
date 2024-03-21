65f90ad7e18b531ac6a76912e717db48c39d901a
pingproject27 <125154974+pingproject27@users.noreply.github.com>
2024-03-21 01:02:03 -0600

    Create SS_AverageSalaries.sql

---
+++ SS_AverageSalaries.sql
@@ -0,0 +1,5 @@
+-- Compare each employee's salary with the average salary of the corresponding department.
+-- Output the department, first name, and salary of employees along with the average salary of that department.
+
+select department, first_name, salary, avg(salary) over(partition by department) as avg
+from employee
